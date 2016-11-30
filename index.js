0121fecb8d154fedc1bc5caa9361532203460065
simonmarklar <simondobie23@gmail.com>
2016-11-30 15:21:39 +1100

    FT-4 # Make normalisation transducer - simplify the framework specific part so that it does not have to be a   transducer - make new form controller template

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 // helpers
 const writeSite = require('./lib/write-site.js').writeSite
 const templateService = require('./lib/utils/template-service.js')
-const writeFormSchema = require('./lib/json-schema/write-form-schema.js')
+// we will eventually need to be able to dynamically require this (eg for angular2 or react transforms)
 const formsTransducer = require('./lib/transducers/angular1.5/form-transducer.js').processForm
-const EVENT_NAMES = require('./lib/transducers/angular1.5/form-transducer.js').EVENT_NAMES
+const normalisationTransducer = require('./lib/transducers/normalisation.js').normaliseForm
 
 // get forms from a live answerspace
 const getAnswerspaceId = require('./lib/utils/answerspace/fetch-answerspace-id.js')
@@ -14,18 +14,19 @@ const getFormDefinition = require('./lib/utils/answerspace/fetch-forms.js')
 const logError = (err) => console.log(err)
 
 // make angular elements transforms
-function transform (options) {
+function normalise (options) {
   const answerspace = options.answerspace
   return Promise.all([
     getAnswerspaceId(answerspace).then(getFormDefinition),
     templateService.load(options.templates)
-  ]).then((data) => formsTransducer(data[0]))
+  ]).then((data) => data[0].map((f) => normalisationTransducer(f)))
 }
 
 function compile (options) {
   const outputPath = options.outputPath
 
-  return transform(options)
+  return normalise(options)
+    .then((normalisedForms) => formsTransducer(normalisedForms))
     .then((formData) => writeSite(outputPath, formData))
     .then((formData) => {
       const gulp = require('gulp')
