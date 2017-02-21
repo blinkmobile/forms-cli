7e04e76d8b24e408bde311635a2e1c03dbbb6886
simonmarklar <simondobie23@gmail.com>
2017-02-22 09:51:54 +1100

    fix prompt flow when overwrite=false

--- commands/init.js
+++ commands/init.js
@@ -1,10 +1,22 @@
 'use strict'
 
+const log = require('../lib/logger.js').logger
+
 const askQuestions = require('../lib/init/ask-questions.js')
-const writeConfig = require('../lib/init/write-config.js')
+const writeConfig = require('../lib/config/write-config.js')
 
 function init () {
-  return askQuestions().then(writeConfig)
+  return askQuestions()
+    .then(writeConfig)
+    .catch((err) => {
+      if (err.message.toLowerCase() === 'cancelled') {
+        log.debug('User cancelled operation')
+        return Promise.resolve()
+      }
+
+      // make sure the error message gets up to the top level promise#catch
+      return Promise.reject(err)
+    })
 }
 
 module.exports = init
