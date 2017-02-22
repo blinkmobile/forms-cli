a5732f28c980644e6e5d0ae07cb94adf5142f646
simonmarklar <simondobie23@gmail.com>
2017-02-22 14:38:28 +1100

    Added writing of template files to target folder

--- commands/init.js
+++ commands/init.js
@@ -4,10 +4,12 @@ const log = require('../lib/logger.js').logger
 
 const askQuestions = require('../lib/init/ask-questions.js')
 const writeConfig = require('../lib/config/write-config.js')
+const writeTemplates = require('../lib/init/write-templates.js')
 
 function init () {
   return askQuestions()
     .then(writeConfig)
+    .then(writeTemplates)
     .catch((err) => {
       if (err.message.toLowerCase() === 'cancelled') {
         log.debug('User cancelled operation')
