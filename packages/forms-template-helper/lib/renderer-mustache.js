fad0bfa018e4d10fe0a8a4007c2d2fb7b7919527
simonmarklar <simondobie23@gmail.com>
2017-03-23 17:15:10 +1100

    FT-77 # Fix template loading issue caused by refactoring

--- packages/forms-template-helper/lib/renderer-mustache.js
+++ packages/forms-template-helper/lib/renderer-mustache.js
@@ -5,16 +5,18 @@ const co = require('co')
 
 const readContents = require('./read-file-contents.js').readContents
 
+const renderer = (template) => {
+  mustache.parse(template, [ '<%', '%>' ])
+
+  return (el) => mustache.render(template, el)
+}
+
 const createRenderer = co.wrap(function * (templatePath) {
   const templateString = yield readContents(templatePath)
-  return mustache(templateString)
+  return renderer(templateString)
 })
 
 module.exports = {
   createRenderer,
-  renderer: (template) => {
-    mustache.parse(template, [ '<%', '%>' ])
-
-    return (el) => mustache.render(template, el)
-  }
+  renderer
 }
