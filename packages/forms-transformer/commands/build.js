7f78166f4384b6f76b2d83c5cef1fa4e1be2d9be
simonmarklar <simondobie23@gmail.com>
2017-03-29 12:38:11 +1100

    change require call to use the plugin loader

--- packages/forms-transformer/commands/build.js
+++ packages/forms-transformer/commands/build.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const readConfig = require('../lib/config/read-config.js')
+const loadPlugin = require('../lib/plugin-system/load-plugin.js')
 
 function build () {
   return readConfig().then((cfg) => {
-    const plugin = require(cfg.framework)
+    const plugin = loadPlugin(cfg.framework)
     return plugin.build(cfg).then(() => ({options: cfg}))
   })
 }
