562e6b3e33b9af2061aef402ca09a0afcd5536d9
simonmarklar <simondobie23@gmail.com>
2017-05-18 11:16:37 +1000

    FT-92 # Default values not working as expected - fixed default values not coming through as expected javascript types - added tests

--- packages/forms-transformer/lib/transforms/set-defaults.js
+++ packages/forms-transformer/lib/transforms/set-defaults.js
@@ -4,10 +4,19 @@ function extractDefaults (el) {
   switch (el.type) {
     case 'text':
     case 'textarea':
-    case 'date':
-    case 'datetime':
-    case 'time':
-      el.value = el.defaultValue ? `'${el.defaultValue}'` : undefined
+    case 'password':
+    case 'radio':
+    case 'email':
+    case 'url':
+      el.value = el.defaultValue ? JSON.stringify(el.defaultValue) : undefined
+      break
+    case 'checkboxes':
+    case 'select':
+      if (el.multi) {
+        el.value = `[${JSON.stringify(el.defaultValue)}]`
+      } else {
+        el.value = el.defaultValue ? JSON.stringify(el.defaultValue) : undefined
+      }
       break
     default:
       el.value = el.defaultValue
