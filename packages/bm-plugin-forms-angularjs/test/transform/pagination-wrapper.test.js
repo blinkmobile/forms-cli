32fbd4e648d6de31783c6d28888f4d49ac99fe5f
simonmarklar <simondobie23@gmail.com>
2017-05-22 12:28:09 +1000

    FT-95 # fix tests for pagination

--- packages/bm-plugin-forms-angularjs/test/transform/pagination-wrapper.test.js
+++ packages/bm-plugin-forms-angularjs/test/transform/pagination-wrapper.test.js
@@ -8,12 +8,12 @@ const test = require('ava')
 const TEST_SUBJECT = '../../lib/transform/pagination-wrapper-template.js'
 
 test('should insert the html inside the wrapper', (t) => {
-  const expected = `<div class='bm-forms__pagination' ng-show='$ctrl.page === 0'>
+  const expected = `<div class='bm-forms__pagination' ng-show='myCtrl.page === 0'>
   hello, world
 </div>`
 
   const m = require(TEST_SUBJECT)
-  const result = m`${'hello, world'}${0}`
+  const result = m`${'hello, world'}${0}${'myCtrl'}`
 
   t.is(result, expected)
 })
