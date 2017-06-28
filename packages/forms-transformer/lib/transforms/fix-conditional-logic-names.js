43dca4d011886c04562f2f90e4c35100c810e8ee
simonmarklar <simondobie23@gmail.com>
2017-06-28 10:45:59 +1000

    add comment to regex

--- packages/forms-transformer/lib/transforms/fix-conditional-logic-names.js
+++ packages/forms-transformer/lib/transforms/fix-conditional-logic-names.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// This regex replaces any numbers at the start of a string and anything that isn't
+// a character, number or underscore in the tail of the string with ''
 const fixName = (str) => str.replace(/^[0-9]|[^a-z0-9_]/ig, '')
 
 function fixBehaviorChecks (el) {
